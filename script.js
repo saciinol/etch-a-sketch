adf0772078dd42e19103cb71002d64a28b9785fc
saciinol <darrenicolas18@gmail.com>
2025-09-09 15:39:45 +0800

    finished

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
 const container = document.querySelector("#container");
+let sides = 16;
 
-function createDiv() {
-   const div = document.createElement("div");
-	div.classList.add("box");
-	container.appendChild(div);
+function createGrid(side) {
+	container.innerHTML = "";
+	const area = side * side;
 
-   div.addEventListener("mouseenter", () => {
-      div.classList.add("box-hover");
-   })
+	function createDiv() {
+		const div = document.createElement("div");
+		div.classList.add("box");
 
-   div.addEventListener("mouseleave", () => {
-      div.classList.remove("box-hover");
-   })
+		div.style.flex = `0 0 calc(${100 / side}%)`;
+
+		container.appendChild(div);
+
+		div.addEventListener("mouseenter", () => {
+			div.classList.add("box-hover");
+		});
+
+		div.addEventListener("mouseleave", () => {
+			div.classList.remove("box-hover");
+		});
+	}
+
+	for (let i = 0; i < area; i++) {
+		createDiv();
+	}
 }
 
-for (let i = 0; i < 256; i++) {
-   createDiv();
-}
+createGrid(sides);
 
+const button = document.querySelector("#newGridBtn");
+const overlay = document.querySelector("#overlay");
+const newGrid = document.querySelector("#newGrid");
+const inputSide = document.querySelector("#inputSide");
+
+button.addEventListener("click", () => {
+	overlay.classList.add("active");
+	inputSide.focus();
+});
+
+overlay.addEventListener("click", (e) => {
+	if (e.target !== newGrid && e.target !== inputSide) {
+		overlay.classList.remove("active");
+	}
+});
+
+window.addEventListener("keydown", (e) => {
+	if (e.key === "Escape") {
+		overlay.classList.remove("active");
+	}
+});
+
+inputSide.addEventListener("keydown", (e) => {
+	if (e.key === "Enter" && e.target.value <= 100) {
+		sides = e.target.value;
+		createGrid(sides);
+		overlay.classList.remove("active");
+		e.target.value = "";
+	}
+});
